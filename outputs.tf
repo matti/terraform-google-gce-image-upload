5b5b05fa9f8b3fe8b4afbc2cf4fb1b61e077f67c
Matti Paksula <matti.paksula@iki.fi>
2018-02-22 15:03:38 +0200

    feat (image) image name output.

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "id" {
   value = "${google_compute_image.image.id}"
 }
 
-output "self_link" {
-  value = "${google_compute_image.image.self_link}"
+output "image_name" {
+  value = "${google_compute_image.image.name}"
 }
